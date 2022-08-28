9de02d8bb33a1f74d47b27c04f672c57ad14beb4
shloksuman <shloksuman01@gmail.com>
2022-08-28 14:06:15 +0530

    comments added

--- sorting/selectionsort.cpp
+++ sorting/selectionsort.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 void selectionsort(int arr[] , int n){
     for(int i= 0 ; i<n ; i++){
-        int minIndex = i ;
+        int minIndex = i ;              //the initial index where we will place the smallest value , if found in the remaining array
         for(int j= i+1 ; j<n ; j++){
-            if(arr[minIndex]>arr[j]){
+            if(arr[minIndex]>arr[j]){   //finding value smaller than the one at minIndex - minIndex denotes the index having minimum value till now
                 minIndex=j;
             }
-        }
-        swap(arr[minIndex] , arr[i]) ;
-    }
+        }                               //at the end of the loop , we have the Index of Minimum value in the unsorted
+        swap(arr[minIndex] , arr[i]) ;  // now we swap the initial value , i , with the newly found(if not found ,
+    }                                   //  then the element is swapped with itself) minIndex's element value
 }
 
 void printarray(int arr[] , int size){
