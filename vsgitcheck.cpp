89622af72fb47379ab0d40c833af248fdab68fcc
shloksuman <shloksuman01@gmail.com>
2022-08-28 09:31:21 +0530

    first commit

---
+++ vsgitcheck.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    cout << "This is file is made for the sole purpose of checking the integration of Git with VSCode!" << endl;
+    
+    return 0 ;
+}
