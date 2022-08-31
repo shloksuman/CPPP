890adbaaeab44689a19dd54290d688f46e31624e
shloksuman <shloksuman01@gmail.com>
2022-08-31 08:20:07 +0530

    removed comments

--- sorting/insertionsort.cpp
+++ sorting/insertionsort.cpp
@@ -4,18 +4,17 @@ using namespace std;
 void insertionsort(int arr[] , int n){
     for(int i= 1 ; i<n ; i++){
         int temp = arr[i] ; 
-        int j= i-1 ;            //the initial index where we will place the smallest value , if found in the remaining array
+        int j= i-1 ;            
         for( ; j>=0 ; j--){
-            if(arr[j]>temp){   //finding value smaller than the one at minIndex - minIndex denotes the index having minimum value till now
+            if(arr[j]>temp){   
                 arr[j+1]= arr[j];
             }
             else break;
-        }                               //at the end of the loop , we have the Index of Minimum value in the unsorted
-        arr[j+1] = temp  ;  // now we swap the initial value , i , with the newly found(if not found ,
-                                        //  then the element is swapped with itself) minIndex's element value
+        }                      
+        arr[j+1] = temp  ;  
+                                    
     }
-    //swap(&arr[minIndex],&arr[i]);     //important line to code while submitting online on gfg ... the '&' is important
-    }                                   //otherwise it LEADS TO SEGMENTATION FAULT
+}                                   
 
 void printarray(int arr[] , int size){
     for(int i=0 ; i<size;i++){
