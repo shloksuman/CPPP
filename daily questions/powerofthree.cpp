c952439e096526e244bf7b71dd6680eacc8c1b0d
shloksuman <shloksuman01@gmail.com>
2022-09-08 09:58:50 +0530

    power of two added

--- daily questions/powerofthree.cpp
+++ daily questions/powerofthree.cpp
@@ -2,6 +2,31 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+bool isPowerOfTwo(int n) {
+      
+    
+        if(n<=0){
+            return false ;
+        }
+        if(n==1){
+            return true;
+        }
+        int x = 1 ;
+        unsigned long long int i=1;
+        while(i<=n){
+            i=i*2;
+            x++;
+            if(n==i){
+                cout << "x = " << x-1 << endl ;
+                return true;
+            }
+        }
+        
+        
+    return false ;
+
+}
+
  bool isPowerOfThree(int n) {
       
     
@@ -29,8 +54,12 @@ using namespace std;
 
 int main(){
   //  cout << "enter n : " ;
-    int n ;
+    int n , m ;
+    cout << "enter n (to see if it's power of 3) : ";
     cin >> n ;
     cout << isPowerOfThree(n) << endl;
+    cout << "enter m (to see if it's power of 2) : ";
+    cin >> m ;
+    cout << isPowerOfTwo(m) << endl;
 return 0;
 }
