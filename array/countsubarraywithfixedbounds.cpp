e86daa83d388e5f1a68d657efb281885c314224f
shloksuman <shloksuman01@gmail.com>
2023-03-04 19:31:18 +0530

    count subarrays with fixed bounds

--- array/countsubarraywithfixedbounds.cpp
+++ array/countsubarraywithfixedbounds.cpp
@@ -10,6 +10,7 @@ long long countSubarrays(vector<int> &nums, int minK, int maxK)
 
     for (int i = 0; i < nums.size(); ++i)
     { // iterate over every element in nums
+    
         if (nums[i] < minK || nums[i] > maxK)
             j = i; // if nums[i] is out of range, move starting index of current subarray to i
         if (nums[i] == minK)
@@ -19,6 +20,7 @@ long long countSubarrays(vector<int> &nums, int minK, int maxK)
 
         // calculate number of valid subarrays that end at index i and add to answer
         ans += max(0, min(prevMinKIndex, prevMaxKIndex) - j);
+        cout << "i : " << i << " MinI : " << prevMinKIndex << " MaxI : " << prevMaxKIndex << " j : " << j << " ans : " << ans <<  endl;
     }
 
     return ans; // return the total count of valid subarrays
@@ -26,9 +28,12 @@ long long countSubarrays(vector<int> &nums, int minK, int maxK)
 
 int main()
 {
-    vector<int> nums = {1, 3, 5, 2, 7, 5};
-    int ans = countSubarrays(nums, 1, 5);
+    vector<int> nums = {4,2,3,5,2,3,4,5};
+    int ans = countSubarrays(nums, 2, 5);
     cout << "no of possible subarrays is/are : " << ans << endl;
+    vector<int> nums2 = {1, 1,1,1};
+    int ans2 = countSubarrays(nums2, 1, 1);
+    cout << "no of possible subarrays is/are : " << ans2 << endl;
 
     return 0;
 }
