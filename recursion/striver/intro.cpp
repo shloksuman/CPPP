d8a6b8d654fc77245fa8255dfb25ca75aa20e8ef
shloksuman <shloksuman01@gmail.com>
2023-08-13 14:37:42 +0530

    new file added

---
+++ recursion/striver/intro.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+#define ll long long
+
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--){
+
+    }
+return 0;
+}
