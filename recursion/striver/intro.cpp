092e437e27deba9a8b61b86fd7d7f31979d6f983
shloksuman <shloksuman01@gmail.com>
2023-08-13 16:19:14 +0530

    added an intro of recursion file

--- recursion/striver/intro.cpp
+++ recursion/striver/intro.cpp
@@ -1,9 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
 void print(){
     cout << 1 << endl;
     print();
+} //this function doesn't have BASE CONDTION
+// so it runs till it runs out of memory and gives segmentation fault
+
+int cnt=0;
+
+void print2(){
+    if(cnt==6){
+        return;
+    }
+
+    cout << cnt << endl;
+    cnt++;
+
+    print2();
+
 }
 
 int main(){
@@ -11,7 +27,9 @@ int main(){
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
     #endif
-    print();
+
+    //print();
+    print2();
 
 return 0;
 }
