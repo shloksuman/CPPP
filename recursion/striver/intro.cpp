f5c787715fb00e4a61c2b7d633027af5c5d5de44
shloksuman <shloksuman01@gmail.com>
2023-08-13 14:45:40 +0530

    another commit

--- recursion/striver/intro.cpp
+++ recursion/striver/intro.cpp
@@ -1,15 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define endl "\n"
-#define ll long long
+void print(){
+    cout << 1 << endl;
+    print();
+}
 
 int main(){
-    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-    int t;
-    cin>>t;
-    while(t--){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    print();
 
-    }
 return 0;
 }
