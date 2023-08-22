cb9a59951a7468b462268f42bc5c9f50c541bf6e
shloksuman <shloksuman01@gmail.com>
2023-08-22 12:21:43 +0530

    created mallocfree file

---
+++ pointers/mallocFree.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+#define ll long long
+
+int main(){
+    
+    int *ptr = (int *)malloc(sizeof(int)); // dynamically allocated memory in heap
+
+    cout << "enter value to get stored in address pointed by ptr which is allocated to it via dynamic memory allocation using malloc function : " ;
+    cin >> *ptr ;
+
+    cout << "*ptr = " << *ptr << endl;
+
+    cout << "address of ptr is : " << ptr << endl;
+
+    free(ptr); // de allocates the allocated memory
+
+
+return 0;
+}
--- pointers/pointersbasics.cpp
+++ pointers/pointersbasics.cpp
@@ -6,27 +6,27 @@ void func(int *ptr) {
 }
 
 int main(){
-//    int a=3 ;
-//     int *p= &a;
-//    // cout << sizeof(p) ;
-//  //   cout << *p ;
-//  cout << a << endl; 
+    int a=3 ;
+    int *p= &a;
+    cout << sizeof(p) << endl ;
+    cout << *p << endl;
+    cout << a << endl; 
 
-//  cout << &a << endl;
-//  cout << p << endl;
-//  cout << &p << endl;
-//  //cout << *a << endl;
+    cout << &a << endl;
+    cout << p << endl;
+    cout << &p << endl;
+    cout << *p << endl;
 
-int a =5 ;
-int *p = &a;
+// int a =5 ;
+// int *p = &a;
 
-cout << "before " << p << endl ; 
-cout << "before " << *p << endl ; 
+// cout << "before " << p << endl ; 
+// cout << "before " << *p << endl ; 
 
-func(p) ;
+// func(p) ;
 
-cout << "after " << p << endl ;
-cout << "after " << *p << endl ;
+// cout << "after " << p << endl ;
+// cout << "after " << *p << endl ;
 
 
 
