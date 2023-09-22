2da7bc19142ebeba62e7e9920054349ba58ac44e
Shlok Kumar Suman <shloksuman01@gmail.com>
2023-09-22 23:23:48 +0530

    added merge code

--- linkedList/basic.cpp
+++ linkedList/basic.cpp
@@ -1,10 +1,43 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+
+
+  ListNode* merge(ListNode* list1, ListNode* list2){
+   
+
+  }
+
+  ListNode* mergeTwoLists(ListNode* list1, ListNode* list2){
+
+  }
+
 int main(){
 
-    cout << "hello bhai! going to learn linked list ";
+    ListNode* h1 = NULL;
+    ListNode* h2 = NULL;
+    int arr1[] = { 1 , 4 , 5 ,7 } ;
+    int arr1[] = { 2 , 3 , 6 } ;
+    for(int i=0; i<4;i++){
+        insertAtTail(head1, arr1[i]);
+    }
+    for( int i=0 ; i<3 ; i++ ){
+        insertAtTail(head2, arr2[i]);
+    }
+
+    display(head1) ;
+    display(head2) ;
+
+    ListNode* newhead = merge(h1 , h2);
+    display(newhead);
 
     return 0;
-    
+
 }
