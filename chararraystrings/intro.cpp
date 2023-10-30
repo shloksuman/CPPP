c24753bf3fa7932d860ca829bf3a01c8dbc02f25
Shlok Kumar Suman <shloksuman01@gmail.com>
2023-10-30 19:10:42 +0530

    slight

--- chararraystrings/intro.cpp
+++ chararraystrings/intro.cpp
@@ -1,11 +1,27 @@
 #include<iostream>
 #include<string>
+#include<algorithm>
 
 using namespace std;
 
 int main(){
 
-    string str;
+    string str = "mnabcdk";
+
+    cout << "Initial string : " << str << endl;
+
+    sort(str.begin() , str.end());
+
+    cout << "\nSorted string str is : " << str << endl;
+
+    str.erase(2,4);
+    cout << "\nAfter erase(), str : " << str << endl;
+
+    str.clear() ;
+    cout << "\nAfter clear() , str : " << str << endl;
+
+    string s = "abcdemn" ;
+    cout << "\nSubtring of  s is : " << s.substr(2,4) << endl;
 
     cout << "\nThe file is working!!!" << endl;
 
